11de142a2e3cf3cac21dc58e408a77f59ad18fc5
hkryucr <hkryucr@gmail.com>
2019-06-24 12:37:06 -0700

    created a folder and did first coding challenge

---
+++ leetcode/src/377_CombinationSumIV.js
@@ -0,0 +1,59 @@
+/*
+Given an integer array with all positive numbers and no duplicates, find the number of possible combinations that add up to a positive integer target.
+
+Example:
+
+nums = [1, 2, 3]
+target = 4
+
+The possible combination ways are:
+(1, 1, 1, 1)
+(1, 1, 2)
+(1, 2, 1)
+(1, 3)
+(2, 1, 1)
+(2, 2)
+(3, 1)
+
+Note that different sequences are counted as different combinations.
+
+Therefore the output is 7.
+ 
+Follow up:
+What if negative numbers are allowed in the given array?
+How does it change the problem?
+What limitation we need to add to the question to allow negative numbers?
+
+Credits:
+Special thanks to @pbrother for adding this problem and creating all test cases.
+
+*/
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+const combinationSum4 = (nums, target) => {
+  if(nums.length == 0) return 0;
+  if(target == 0) return 0;
+  let numberOfEle = 0;
+
+  function findAll(arr){
+    if(arr.reduce(function(a,b){return a+b},0) == target){
+      numberOfEle ++
+    }
+    if(arr.reduce(function(a,b){return a+b},0) > target){
+      return
+    }
+    nums.forEach(el => {
+      // arr.concat creates new arrays everytime.
+      findAll(arr.concat(el));
+    });
+  }
+  findAll([])
+
+  return numberOfEle;
+};
+
+console.log(combinationSum4([4,2,1], 10))
