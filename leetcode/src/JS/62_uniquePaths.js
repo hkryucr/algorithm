4cc8573b1e764e5e616303fc799d0240a26359b0
hkryucr <hkryucr@gmail.com>
2019-07-02 18:33:17 -0700

    added 62_uniquePaths.js

--- leetcode/src/JS/62_uniquePaths.js
+++ leetcode/src/JS/62_uniquePaths.js
@@ -12,11 +12,24 @@ How many possible unique paths are there?
  * @return {number}
  */
 
+/*
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+*/
+
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+
 const uniquePaths = (m, n) => {
   // Create a memo 
   let memo =[[]];
 
-  // check the base
   if(m == 0 || n == 0) return null;
   if(m == 1 && n == 1) return 1;
 
