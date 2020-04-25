d7b1d3382cd9445463a05e0b673d5d92d0648f47
Henry Ryu <hkryucr@gmail.com>
2020-04-25 01:26:53 -0700

    rotatingOranges

--- CodingInterviewPrep/Q9.java
+++ CodingInterviewPrep/Q9.java
@@ -36,7 +36,11 @@ w x y x
 a b c
 d e f
 
+f
 
+letterCombinations("3")     letterCombinations("2") ["a","b","c"]
+    ["d","e","f"]       *        ["a","b","c"] =>       letterCombinations("32") => [ad ae af bd be df cd ce cf]
+    ["g", "h","i"]       *       ( ["d","e","f"]       *        ["a","b","c"] ) =>       letterCombinations("4     32")
 
 
 Although the above answer is in lexicographical order, your answer could be in any order you want.
@@ -45,7 +49,46 @@ Although the above answer is in lexicographical order, your answer could be in a
 
 class Solution {
     public List<String> letterCombinations(String digits) {
+        List<String> res = new ArrayList<>();
+        if(digits==null||digits.length==0) return res;
+        //int i = 0
+        List<String> ans = new ArrayList<>();
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(0, "");
+        map.put(1, "");
+        map.put(2, "abc")
+        map.put(3, "def");
+        map.put(4, "abc")
+
+
+        return backtrack(digits, 0, ans, res, map);
 
         
+    }
+
+    public List<String> backtrack(String digits, String tmpString, int curr, List<String> tmp, List<String> res, HashMap<Integer, String> map){
+        if(curr==digits.length()) return res;
+        if
+        
+
+
+        // for(int i=0; i<digits.length(); i++){
+            // "123" -> "12" ["ab","ba", "ce", "..... "....] backtrack returns arr
+            List<String> prevArr = backtrack("12" , )
+            // char ch = digits.charAt(i); // 3
+            char ch = digits[digits.length()-1; // 3
+            int num = Integer.parseInt(ch);
+            String mapped = map.get(num); // "def"
+            List<String> tempArr = new ArrayList<>();
+            for(int j = 0 ; j<prevArr.length(); j++){
+                // char ch = prevArr.charAt(0);
+                prevArr // "ab"
+                for(String word: ans){
+                    ans.add(word+ch); // "abd" , "abe", "abf"
+                    tempArr.add(word+ch);
+                }
+            }
+            return tempArr;
+        // }
     }
 }
