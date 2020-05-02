1c203db7155578bd8c5d7bd33fb515c81dd76ff3
Henry Ryu <hkryucr@gmail.com>
2020-05-01 21:58:41 -0700

    kill process

---
+++ leetcode/src/582_killProcess.js
@@ -0,0 +1,50 @@
+/**
+ * Given n processes, each process has a unique PID (process id) and its PPID (parent process id).
+
+Each process only has one parent process, but may have one or more children processes. This is just like a tree structure. Only one process has PPID that is 0, which means this process has no parent process. All the PIDs will be distinct positive integers.
+
+We use two list of integers to represent a list of processes, where the first list contains PID for each process and the second list contains the corresponding PPID.
+
+Now given the two lists, and a PID representing a process you want to kill, return a list of PIDs of processes that will be killed in the end. You should assume that when a process is killed, all its children processes will be killed. No order is required for the final answer.
+
+Example 1:
+Input: 
+pid =  [1, 3, 10, 5]
+ppid = [3, 0, 5, 3]
+kill = 5
+Output: [5,10]
+Explanation: 
+           3
+         /   \
+        1     5
+             /
+            10
+Kill 5 will also kill 10.
+ */
+
+
+var killProcess = function (pid, ppid, kill) {
+    let hash = {};
+    let res = [];
+
+    for (let i = 0; i < pid.length; i++) {
+        let curPid = pid[i];
+        let curParentId = ppid[i];
+        if (hash[curParentId] === undefined) hash[curParentId] = [];
+        hash[curParentId].push(curPid);
+    }
+
+    killAll(kill, res, hash)
+    return res;
+};
+
+function killAll(ppid, res, hash) {
+    res.push(ppid);
+    let children = hash[ppid];
+    if (children === undefined) return;
+
+    for (let i = 0; i < children.length; i++) {
+        let child = children[i];
+        killAll(child, res, hash);
+    }
+}
