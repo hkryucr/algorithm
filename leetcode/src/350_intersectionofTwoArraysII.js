043ee42cbfcbd37b8d66e5ce094035ee98b694c9
Henry Ryu <hkryucr@gmail.com>
2020-05-13 19:14:07 -0700

    350. Intersection of Two Arrays II

---
+++ leetcode/src/350_intersectionofTwoArraysII.js
@@ -0,0 +1,49 @@
+/*
+Given two arrays, write a function to compute their intersection.
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+Note:
+
+Each element in the result should appear as many times as it shows in both arrays.
+The result can be in any order.
+Follow up:
+
+What if the given array is already sorted? How would you optimize your algorithm?
+What if nums1's size is small compared to nums2's size? Which algorithm is better?
+What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+
+*/
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+
+var intersect = function (nums1, nums2) {
+    //solution 1 (Sort (nlongn+mlogm + (m+n)))
+    nums1.sort((a, b) => a - b);
+    nums2.sort((a, b) => a - b);
+    let i = 0;
+    let j = 0;
+    let res = [];
+
+    while (i < nums1.length && j < nums2.length) {
+        if (nums1[i] === nums2[j]) {
+            res.push(nums1[i]);
+            i++;
+            j++;
+        } else if (nums1[i] > nums2[j]) {
+            j++;
+        } else {
+            i++;
+        }
+    }
+    return res;
+};
