34a88e7ab495c8e07dc90eab31c306b8254184e9
Henry Ryu <hkryucr@gmail.com>
2020-05-19 23:10:47 -0700

    1289_minimumFallingPathSumII.js

--- henry.js
+++ henry.js
@@ -1,84 +1,153 @@
 /*
-https://leetcode.com/problems/subarray-sum-equals-k/
-https://leetcode.com/problems/number-of-submatrices-that-sum-to-target/
-Given an array of integers and an integer k, you need to find the total number of continuous subarrays 
-whose sum equals to k.
+Design a Tic-tac-toe game that is played between two players on a n x n grid.
 
-Example 1:
+You may assume the following rules:
 
-Input:nums = [1,1,1], k = 2
-Output: 2
+A move is guaranteed to be valid and is placed on an empty block.
+Once a winning condition is reached, no more moves is allowed.
+A player who succeeds in placing n of their marks in a horizontal, vertical, or diagonal row wins the game.
+Example:
+Given n = 3, assume that player 1 is "X" and player 2 is "O" in the board.
 
- 0 1
-[1,1]
- 1 2
-[1,1]
-   v v
-         v 
-[1,2,3,4,5,6]
-k = 5
-2
+TicTacToe toe = new TicTacToe(3);
 
-// el in the array can be any integer;
-// k can be any integer;
+toe.move(0, 0, 1); -> Returns 0 (no one wins)
+|X| | |
+| | | |    // Player 1 makes a move at (0, 0).
+| | | |
 
-Naive approach -> use nested for loop -> find all continuous subarrays and find elements whose sum is K
+toe.move(0, 2, 2); -> Returns 0 (no one wins)
+|X| |O|
+| | | |    // Player 2 makes a move at (0, 2).
+| | | |
 
-i = 0
-j = 1 
-=> [1,2] -> see its sum
-n^2 * n => n^3
-no space required for this.
+toe.move(2, 2, 1); -> Returns 0 (no one wins)
+|X| |O|
+| | | |    // Player 1 makes a move at (2, 2).
+| | |X|
 
-[1,2,3,4,5,6]
+toe.move(1, 1, 2); -> Returns 0 (no one wins)
+|X| |O|
+| |O| |    // Player 2 makes a move at (1, 1).
+| | |X|
 
-[1,2,3]
-              1 2 3 4   3  5 
-Input:nums = [1,1,1,1, -1, 2,1], k = 2
-=> 3
-nums =     [1, 1, 1] k = 2
-            i     j 
-         0  1  2  3
-         0 1 2 3 4 3 4
+toe.move(2, 0, 1); -> Returns 0 (no one wins)
+|X| |O|
+| |O| |    // Player 1 makes a move at (2, 0).
+|X| |X|
 
-prefix =[0, 1, 2, 3, 1, 3, 1]
-prefix[i] - k =  prefix[j];
- i > j
-i < j < n < l
-sum(nums[i:l]) == k
-sum(nums[j:n]) == k
-k = 2
-hash = {
-    0: 1
-    1: 1,
-    2: 2
-    3: 2
-}
+toe.move(1, 0, 2); -> Returns 0 (no one wins)
+|X| |O|
+|O|O| |    // Player 2 makes a move at (1, 0).
+|X| |X|
 
-prefix[3] = sum of all number in nums 0 to 2
-prefix[1] = sum of all number in nums 0 to 0
-
-prefix[i] = sum(nums[0:i])
-prefix[j] = sum(nums[0:j])
-
-can you get sum(nums[i:j]) using prefix?
-
-sum(nums[i:j]) == k
-
-sum(nums[i:j]) = prefix[i], prefix[j]
-i = 6 j = 3
-            v   v v  v v v v
-prefix = > [5,-10,6,-1,5,6,7]
-
-hashmap ={
-    value: counts
-}
-Tabulation
-  0 1 2  i
-0 1 0 0  
-1 3 2
-2 6 5 3 
-j
-
+toe.move(2, 1, 1); -> Returns 1 (player 1 wins)
+|X| |O|
+|O|O| |    // Player 1 makes a move at (2, 1).
+|X|X|X|
 */
 
+
+/*
+1) create 2d array => 3x3
+2) move -> find the spot and fill in the player number
+3) every time the user moves, need to check whether there's any winner
+  - check row, col, diagnal -> if all has the same value -> return the player ;
+  - otherwise return 0;
+
+player 1 -> i, j -> +1;
+player 2 -> i, j => -1;
+checking arr
+rowCheck = [];
+colCheck = [];
+diagnalCheck = [];
+row 1 -> 0
+row 2 -> 
+row 3 -> 
+col 1 -> 1
+col 2 -> -1
+col 3 ->
+diag1 => 
+diag2 => 
+
+8 times 
+*/
+/**
+ * Initialize your data structure here.
+ * @param {number} n
+ */
+var TicTacToe = function(n) {
+    this.board = Array.from({length: n}, ()=> new Array(n));
+    this.rowCheck = new Array(3).fill(0);
+    this.colCheck = new Array(3).fill(0);
+    this.digCheck = new Array(2).fill(0);
+};
+
+/**
+ * Player {player} makes a move at ({row}, {col}).
+        @param row The row of the board.
+        @param col The column of the board.
+        @param player The player, can be either 1 or 2.
+        @return The current winning condition, can be either:
+                0: No one wins.
+                1: Player 1 wins.
+                2: Player 2 wins. 
+ * @param {number} row 
+ * @param {number} col 
+ * @param {number} player
+ * @return {number}
+ */
+TicTacToe.prototype.move = function(row, col, player) {
+
+  if(this.board[row][col] !== undefined) return -1;
+  this.board[row][col] = player;
+  let curPlayerNum = (player === 2) ? -1 : player;
+  this.rowCheck[row] += curPlayerNum;
+  this.colCheck[col] += curPlayerNum;
+  if(row === col) this.digCheck[0] += curPlayerNum;
+  if(2 - row === col) this.digCheck[1] += curPlayerNum;
+  
+  let winner = 0;
+  // this.rowCheck.forEach(el => {
+  //   if(el === 3) winner = 1;
+  //   if(el === -3) winner = 2;
+  // })
+  // this.colCheck.forEach((el) => {
+  //   if (el === 3) winner = 1;
+  //   if (el === -3) winner = 2;
+  // });
+  if (this.rowCheck[row] === 3) winner = 1;
+  if (this.rowCheck[row] === -3) winner = 2
+  if (this.colCheck[col] === 3) winner = 1;
+  if (this.colCheck[col] === -3) winner = 2;
+  this.digCheck.forEach((el) => {
+    if (el === 3) winner = 1;
+    if (el === -3) winner = 2;
+  });
+  // console.log(this.board);
+  // console.log(this.digCheck, "digCheck");
+  return winner;
+};
+var obj = new TicTacToe(3)
+console.log(obj.move(0,0,1)) // 0
+console.log(obj.move(0,1,2)) // 0
+console.log(obj.move(1,1,1)) // 0
+console.log(obj.move(0,2,2)) // 0
+console.log(obj.move(2,2,1)) // 1
+
+// console.log(obj.move(0,2,1)) // 0
+// console.log(obj.move(0,0,2)) // 0
+// console.log(obj.move(1,1,1)) // 0
+// console.log(obj.move(0,1,2)) // 0
+// console.log(obj.move(2,0,1)) // 1
+
+row=1,col=1,player=1
+[1, 2, 2]
+[0, 1, 0]
+[0, 0, 1]
+
+/** 
+ * Your TicTacToe object will be instantiated and called as such:
+ * var obj = new TicTacToe(n)
+ * var param_1 = obj.move(row,col,player)
+ */
--- kazuki.py
+++ kazuki.py
@@ -1,92 +1,104 @@
 '''
-Given a string s, partition s such that every substring of the partition is a palindrome.
+We are given a binary tree (with root node root), a target node, and an integer value K.
 
-Return all possible palindrome partitioning of s.
+Return a list of the values of all nodes that have a distance K from the target node.  The answer can be returned in any order.
 
-Example:
+Example 1:
 
-Input: "aab"
-Output:
-[
-  ["aa","b"],
-  ["a","a","b"]
-]
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], target = 5, K = 2
+
+Output: [7,4,1]
+
+Explanation: 
+The nodes that are a distance 2 from the target node (with value 5)
+have values 7, 4, and 1.
+
+Note that the inputs "root" and "target" are actually TreeNodes.
+The descriptions of the inputs above are just serializations of these objects.
+
+Note:
+
+The given tree is non-empty.
+Each node in the tree has unique values 0 <= node.val <= 500.
+The target node is a node in the tree.
+0 <= K <= 1000.
 
 OBSERVATION
-"aab"
+in/post
+target = 5
+k = 2
+values = 7, 4, 1
+                    9
+                    |
+                    3
+            |               |
+            5               1
+    |       |               |     \
+    6    2                   0       8
+        /       \
+        7        4
 
-["a","a","b"]
-["a","a","c","c","a","b"]
+hash table with key=node and value=node's parent
+table = {
+    9: None
+    3: 9
+    5: 3
+    1: 3
+    6: 5
+    2: 5
+    7: 2
+    4: 2
+    0: 1
+    8: 1
+}
 
-  ["aa","c","c","a","b"]
-  ["a","a","cc","a","b"]
+helper(node, target, k)
+9, 5, 2 -> 3, 5, 2
+        -> 9, 5, 0
+        -> 1, 5, 0
+        -> 5, 5, 2
+            -> 6, 5, 1
+            -> 2, 5, 1
+                -> 7, 5, 0
+                -> 4, 5, 0
+'''
 
-T(n) = T(n-1) + T(n-1) + c = 2^n
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
-
-recursive function -> list(s), result, index i and j
-
-"aab"         0    1    2
-palindromes: "a", "a", "b"
-"aa", "a"
-"aaa"
- 0   1    2
-"b" "aa" "b"
-"aab"
-
-result:
-i=0
-palin="aa"
-temp="aa", "b"
-
-"efe"
-[["e","f","e"]]
-[["e","f","e"],["efe"]]
-
-efe but ef or fe are not
-
-
-"efe"
-
--> [["e"]]
--<  
-   [["e", "f"], ["ef"]]
-    "ef" => "efe"
-    "e" "f" => "e" "f" "e"
-         e
-     |              | 
-     e,f            ef
-|       |          |        |
-e,f,e   e,fe       ef,e     
-
-                   i is moving 
-"aaaaaaaaaaaaa     aaaaaaaaa"
-
-""
-'''''
+import collections
 
 class Solution:
-    def partition(self, s: str) -> List[List[str]]:
+    def distanceK(self, root: TreeNode, target: TreeNode, K: int) -> List[int]:
+        table = {}
+        stack = [[root, None]]
+        while stack:
+            node, parent = stack.pop()
+            table[node] = parent
+            if node.left:
+                stack.append([node.left, node])
+            if node.right:
+                stack.append([node.right, node])
+
         result = []
-        self.recursion(list(s), result)
+        queue = collections.deque([target]) # [6 ,2,3]
+        visited = set()
+        while queue:
+            size = len(queue)
+            for _ in range(size):
+                node = queue.popleft()
+                visited.add(node)
+                if table[node] and table[node] not in visited:
+                    queue.append(table[node])
+                if node.left and node.left not in visited:
+                    queue.append(node.left)
+                if node.right and node.right not in visited:
+                    queue.qppend(node.right)
+                if K == 0:
+                    result.append(node.val)
+            K -= 1
         return result
-    
-    def recursion(self, palindromes, result):
-        result.append(list(palindromes))
-        for i in range(len(palindromes) - 1):
-            palin = palindromes[i] + palindromes[i+1]
-            if self.is_palindrome(palin):
-                temp = palindromes[:i] + [palin] + palindromes[i+2:]
-                self.recursion(temp, result)
-
-    def is_palindrome(self, palin):
-        i = 0
-        j = len(palin) - 1
-
-        while i < j:
-            if palin[i] != palin[j]:
-                return False
-            i += 1
-            j -= 1
-        
-        return True
