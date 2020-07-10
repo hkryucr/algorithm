d421025f7b55ea488c8f5ee96478446da0ae5ea2
Henry Ryu <hkryucr@gmail.com>
2020-07-10 10:55:37 -0700

    43. Multiply Strings

---
+++ leetcode/src/43_multiplyStrings.js
@@ -0,0 +1,56 @@
+/*
+Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+
+Example 1:
+
+Input: num1 = "2", num2 = "3"
+Output: "6"
+Example 2:
+
+Input: num1 = "123", num2 = "456"
+Output: "56088"
+Note:
+
+The length of both num1 and num2 is < 110.
+Both num1 and num2 contain only digits 0-9.
+Both num1 and num2 do not contain any leading zero, except the number 0 itself.
+You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+*/
+
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+
+// var multiply = function(num1, num2) {
+//     let arr = new Array(num1.length + num2.length).fill(0);
+//     let revNum1 = num1.split("").reverse();
+//     let revNum2 = num2.split("").reverse();
+    
+//     for (let i = 0; i < revNum1.length; i++) {
+//         for (let j = 0; j < revNum2.length; j++){
+
+//             let curDigit = i + j;
+//             let curMultiplied = parseInt(revNum1[i]) * parseInt(revNum2[j]);
+//             arr[curDigit] += curMultiplied;
+            
+//             while(arr[curDigit] >= 10){
+//                 let carry = Math.floor(arr[curDigit] / 10);
+//                 arr[curDigit] %= 10;
+//                 arr[curDigit+1] += carry;
+//                 curDigit = curDigit+1;
+//             }
+//         }
+//     }
+//     while(arr.length > 1 && arr[arr.length-1] === 0) arr.pop()
+//     return arr.reverse().join("");
+// };
+
+
+var multiply = function (num1, num2){
+    let a=BigInt(num1);
+    let b=BigInt(num2);
+    return String(a*b);
+}
---
+++ leetcode/src/662_maximumWidthofBinaryTree.js
@@ -0,0 +1,95 @@
+/*
+662. Maximum Width of Binary Tree
+
+Given a binary tree, write a function to get the maximum width of the given tree. The width of a tree is the maximum width among all levels. The binary tree has the same structure as a full binary tree, but some nodes are null.
+
+The width of one level is defined as the length between the end-nodes (the leftmost and right most non-null nodes in the level, where the null nodes between the end-nodes are also counted into the length calculation.
+
+Example 1:
+
+Input: 
+
+           1
+         /   \
+        3     2
+       / \     \  
+      5   3     9 
+
+Output: 4
+Explanation: The maximum width existing in the third level with the length 4 (5,3,null,9).
+Example 2:
+
+Input: 
+
+          1
+         /  
+        3    
+       / \       
+      5   3     
+
+Output: 2
+Explanation: The maximum width existing in the third level with the length 2 (5,3).
+Example 3:
+
+Input: 
+
+          1
+         / \
+        3   2 
+       /        
+      5      
+
+Output: 2
+Explanation: The maximum width existing in the second level with the length 2 (3,2).
+Example 4:
+
+Input: 
+
+          1
+         / \
+        3   2
+       /     \  
+      5       9 
+     /         \
+    6           7
+Output: 8
+Explanation:The maximum width existing in the fourth level with the length 8 (6,null,null,null,null,null,null,7).
+
+
+Note: Answer will in the range of 32-bit signed integer.
+
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var widthOfBinaryTree = function(root) {
+    if (root == null) return 0;
+    let max_width = -Infinity;
+    let level = [[root, 0]];
+    while (level.length){
+        let left = Infinity;
+        let right = -Infinity;
+        let newLevel = [];
+        for (let curLevel of level){
+            let [node, idx] = curLevel;
+            left = Math.min(left, idx);
+            right = Math.max(right, idx);
+            if (node.left) newLevel.push([node.left, (idx - left) * 2]);
+            if (node.right) newLevel.push([node.right, (idx-left) * 2 + 1]);
+            level = newLevel;
+        }    
+        max_width = Math.max(right - left + 1, max_width);
+    }
+    return max_width
+};
