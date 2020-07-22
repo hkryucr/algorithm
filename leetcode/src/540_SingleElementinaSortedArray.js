a07087eb9f5449bfbd8363347a1a9e614ddfdce1
Henry Ryu <hkryucr@gmail.com>
2020-07-22 00:10:12 -0700

    304. Range Sum Query 2D - Immutable

--- leetcode/src/540_SingleElementinaSortedArray.js
+++ leetcode/src/540_SingleElementinaSortedArray.js
@@ -24,20 +24,17 @@ Constraints:
  * @return {number}
  */
 var singleNonDuplicate = function(nums) {
-    
     let left = 0;
     let right = nums.length-1;
     
     while(left < right){
         let mid = left + Math.floor((right - left) / 2);
         if(mid % 2 == 1) mid--;
-        
         if(nums[mid] !== nums[mid+1]){
             right = mid;
         } else {
             left = mid+2;
         }        
-    }
-    
+    }    
     return nums[left];
 };
